4be3bfffdbd7b1ed598770b7dd70da6d6a427ff2
Jakaria <jakariait@gmail.com>
2026-04-02 12:07:12 +0600

    Update

--- frontend/src/components/GoogleTagManager.js
+++ frontend/src/components/GoogleTagManager.js
@@ -25,14 +25,14 @@ export default function GoogleTagManager() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N6W7QBL9');
+          })(window,document,'script','dataLayer','GTM-K8RP3DB6');
         `}
       </Script>
 
       {/* Noscript fallback */}
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-N6W7QBL9"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K8RP3DB6"
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
